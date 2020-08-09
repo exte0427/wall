25135805139c90dcdfb737a5c8e4a1437289b6e4
exte0427 <ex20070129@gmail.com>
2020-08-09 17:00:03 +0900

    f

--- index.js
+++ index.js
@@ -489,20 +489,20 @@ class boll{
         this.goingY=gy;
         let nmx=mx;
         const aa=setInterval(()=>{
-            if(this.x<10 && now!=1){
+            if(this.x<10 && nowwww!=1){
                 lastyyy=-1;
                 this.xl*=-1;
-                now=1;
+                nowwww=1;
             }
-            if(this.x>590&& now!=2){
+            if(this.x>590&& nowwww!=2){
                 lastyyy=-1;
                 this.xl*=-1;
-                now=2;
+                nowwww=2;
             }
-            if(this.y<10&& now!=3){
+            if(this.y<10&& nowwww!=3){
                 lastyyy=-1;
                 this.yl*=-1;
-                now=3;
+                nowwww=3;
             }if(this.y>650){
                 if(fd==3){
                     let ppp=0;
@@ -543,7 +543,7 @@ class boll{
             for(let i=0;i<blocksX.length;i++){
                 const pp=checkRectCircleCollision({x:this.x,y:this.y,r:15},{x:blocksX[i],y:blocksY[i],w:50,h:50});
                 if(pp.collised){
-                    now=0;
+                    nowwww=0;
                     if(lastyyy!=i){
                         lastyyy=i;
                         let blockops=blocks.filter(a=>a.delete==0);
